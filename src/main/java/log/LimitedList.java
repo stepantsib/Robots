45406cfa93820f869ceeeb08b65d7dfd1249d123
Stepan <stepantsib@gmail.com>
2026-04-11 16:45:05 +0500

    new Task4

---
+++ src/main/java/log/LimitedList.java
@@ -0,0 +1,35 @@
+package log;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class LimitedList<T> {
+
+    private final int limit;
+    private final LinkedList<T> list;
+
+    public LimitedList(int limit) {
+        this.limit = limit;
+        this.list = new LinkedList<>();
+    }
+
+    public synchronized void add(T element) {
+        while (list.size() > limit) {
+            list.removeFirst();
+        }
+        list.add(element);
+    }
+
+    public synchronized int size() {
+        return list.size();
+    }
+
+    public synchronized Iterable<T> all() {
+        return new ArrayList<>(list);
+    }
+
+    public synchronized Iterable<T> range(int startFrom, int count) {
+        return new ArrayList<>(list).subList(startFrom, count);
+    }
+
+}
