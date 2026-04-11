426e1f9cb9909d6309e7a449c7f2d8ef2602aa0b
Stepan <stepantsib@gmail.com>
2026-04-11 16:48:06 +0500

    formated code

--- src/main/java/log/LogEntry.java
+++ src/main/java/log/LogEntry.java
@@ -1,23 +1,19 @@
 package log;
 
-public class LogEntry
-{
+public class LogEntry {
     private LogLevel logLevel;
     private String strMessage;
-    
-    public LogEntry(LogLevel logLevel, String strMessage)
-    {
+
+    public LogEntry(LogLevel logLevel, String strMessage) {
         this.strMessage = strMessage;
         this.logLevel = logLevel;
     }
-    
-    public String getMessage()
-    {
+
+    public String getMessage() {
         return strMessage;
     }
-    
-    public LogLevel getLevel()
-    {
+
+    public LogLevel getLevel() {
         return logLevel;
     }
 }
